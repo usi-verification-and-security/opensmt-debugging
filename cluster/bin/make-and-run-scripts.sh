1ede6165d386242f2d1220f08296012d7e3e7a82
Antti Hyvarinen <antti.hyvarinen@gmail.com>
2021-08-26 18:34:18 +0200

    Add: cluster submission: Allow running incremental jobs

--- cluster/bin/make-and-run-scripts.sh
+++ cluster/bin/make-and-run-scripts.sh
@@ -11,7 +11,10 @@ DEFAULTOSMT=${DEFAULTOSMT:-~/bin/opensmt}
 DEFAULTCONFIG=empty.smt2
 WORKSCRIPT=${SCRIPT_ROOT}/make_scripts_osmt2.sh
 
-usage="Usage: $0 [-h] [-o <osmt2-binary>] [-c <config>] -b <QF_UF|QF_LRA|QF_LIA> [-f <flavor>]"
+usage="Usage: $0 [-h] [-o <osmt2-binary>] [-c <config>] -b <QF_UF|QF_LRA|QF_LIA|QF_RDL|QF_IDL> [-f <flavor>] [-i true | false] [-m true | false]"
+
+incremental=false;
+produce_models=false;
 
 while [ $# -gt 0 ]; do
     case $1 in
@@ -30,6 +33,12 @@ while [ $# -gt 0 ]; do
         ;;
       -f|--flavor)
         flavor=$2
+        ;;
+      -i|--incremental)
+        incremental=$2
+        ;;
+      -m|--produce-models)
+        produce_models=$2
         ;;
       -*)
         echo "Error: invalid option '$1'"
@@ -73,17 +82,29 @@ if [ ! -f $config ]; then
     exit 1
 fi
 
+if [[ ${incremental} == true ]]; then
+    incr_str="incremental"
+    BMBASE=~/benchmarks-incremental
+else
+    incr_str="non-incremental"
+fi
+
 if [ ${benchmarks} == QF_UF ]; then
     bmpath=${BMBASE}/QF_UF;
 elif [ ${benchmarks} == QF_LRA ]; then
     bmpath=${BMBASE}/QF_LRA;
+elif [ ${benchmarks} == newQF_LRA ]; then
+    bmpath=${BMBASE}/newQF_LRA;
 elif [ ${benchmarks} == QF_LIA ]; then
     bmpath=${BMBASE}/QF_LIA;
+elif [ ${benchmarks} == QF_RDL ]; then
+    bmpath=${BMBASE}/QF_RDL;
+elif [ ${benchmarks} == QF_IDL ]; then
+    bmpath=${BMBASE}/QF_IDL;
 else
     echo "Unknown benchmark ${benchmarks}"
     exit 1
 fi
-
 n_benchmarks=$(ls ${bmpath}/*.smt2.bz2 |wc -l)
 
 echo "Binary:"
@@ -95,8 +116,22 @@ echo " - $(date -r ${binary})"
 echo "Benchmark set (total ${n_benchmarks}):"
 echo " - ${bmpath}"
 
-scriptdir=osmt2-${flavor}-scripts-$(date +'%F')-non-incremental-${benchmarks}
-resultdir=osmt2-${flavor}-results-$(date +'%F')-non-incremental-${benchmarks}
+
+echo "Incremental benchmarks:"
+echo " - ${incremental}"
+
+if [[ ${produce_models} == true ]]; then
+    mv_str="-mv"
+    WORKSCRIPT=${SCRIPT_ROOT}/make_scripts_osmt2_models.sh
+else
+    mv_str=""
+fi
+
+echo "Produce models:"
+echo " - ${produce_models}"
+
+scriptdir=osmt2-${flavor}-scripts-$(date +'%F')-${incr_str}-${benchmarks}${mv_str}
+resultdir=osmt2-${flavor}-results-$(date +'%F')-${incr_str}-${benchmarks}${mv_str}
 
 echo "Work directories:"
 echo " - ${scriptdir}"
