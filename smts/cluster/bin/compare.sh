42d10f9948c3d827d0a372ba25c36bbd8b27a042
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-05-29 20:19:56 +0200

    Refactor smts hpc scripts to be parametric

--- smts/cluster/bin/compare.sh
+++ smts/cluster/bin/compare.sh
@@ -8,6 +8,7 @@ CLUSTER_BIN_ROOT=$(get_abs_path $(dirname $0))
 PLOTTING_BIN_ROOT=${CLUSTER_BIN_ROOT}/../../../plotting/bin
 
 EXTRACTOR=${CLUSTER_BIN_ROOT}/extract_results_time_smts.sh
+echo $EXTRACTOR
 GNUPLOTTOR=${PLOTTING_BIN_ROOT}/make_scatterplot_time.py
 PLOT_MAKEFILE=${PLOTTING_BIN_ROOT}/../Makefile
 
@@ -22,11 +23,19 @@ xd=$1
 yd=$2
 
 
-regex='smts-\(.*\)-results-\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)-\(.*\)-\([A-Z_]*\)'
+regex='\(.*\)-results-\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)-\(.*\)-\([A-Z_]*\)'
 
 x_branch=$(echo ${xd} |sed s/${regex}/\\1/g)
-y_branch=$(echo ${yd} |sed s/${regex}/\\1/g)
+x_branch=(${x_branch//-/ })
+project_x=${x_branch[0]}
+x_branch=${x_branch[1]}
 
+y_branch=$(echo ${yd} |sed s/${regex}/\\1/g)
+y_branch=(${y_branch//-/ })
+project_y=${y_branch[0]}
+y_branch=${y_branch[1]}
+
+echo $y_branch
 x_date=$(echo ${xd} |sed s/${regex}/\\2/g)
 y_date=$(echo ${yd} |sed s/${regex}/\\2/g)
 
@@ -47,8 +56,8 @@ if [ ${x_track} != ${y_track} ]; then
 fi
 
 echo "Extracting "
-[[ ${xd}.list -nt ${xd} ]] || ${EXTRACTOR} ${xd} > ${xd}.list
-[[ ${yd}.list -nt {$yd} ]] || ${EXTRACTOR} ${yd} > ${yd}.list
+[[ ${xd}.list -nt ${xd} ]] || ${EXTRACTOR} ${xd} ${project_x} > ${xd}.list
+[[ ${yd}.list -nt ${yd} ]] || ${EXTRACTOR} ${yd} ${project_y} > ${yd}.list
 echo "done."
 
 
--- smts/cluster/bin/extract_results_time_smts.sh
+++ smts/cluster/bin/extract_results_time_smts.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-if [[ $# == 0 ]]; then
-    echo "Usage: $0 <result-dir>";
+if [[ $# != 2 ]]; then
+    echo "Usage: $0 <result-dir> <project osmt|smts>";
     exit 1;
 fi
 
-for file in $1/smts.*.smt2.bz2.sh.*.out; do
-    name=$(echo $file |sed 's,'$1'/smts\.\(.*\)\.smt2\.bz2\.sh\.\(.*\).out,\1,g');
-    num=$(echo $file |sed 's,'$1'/smts\.\(.*\)\.smt2\.bz2\.sh\.\(.*\).out,\2,g');
+for file in $1/$2.*.smt2.bz2.sh.*.out; do
+    name=$(echo $file |sed 's,'$1'/'$2'\.\(.*\)\.smt2\.bz2\.sh\.\(.*\).out,\1,g');
+    num=$(echo $file |sed 's,'$1'/'$2'\.\(.*\)\.smt2\.bz2\.sh\.\(.*\).out,\2,g');
     inst=$(basename $(head -1 $file) .smt2.bz2);
     if (grep '^sat' $file > /dev/null); then
         result=sat
@@ -16,7 +16,7 @@ for file in $1/smts.*.smt2.bz2.sh.*.out; do
         result=indet
     fi
     dn=$(dirname $file)
-    tf=${dn}/smts.${name}.smt2.bz2.sh.smts.${num}.time
+    tf=${dn}/$2.${name}.smt2.bz2.sh.$2.${num}.time
     tm=$(sed -n 's/.* wall: \(.*\) CPU: .*/\1/p' $tf)
     echo $inst $result $tm;
 done
