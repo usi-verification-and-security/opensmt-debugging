e2078ff70c2188e5d2637fad1dfd14f9c0e0dc90
Antti Hyvärinen <antti.hyvarinen@gmail.com>
2022-06-02 14:29:09 +0200

    model-validation: improvements

--- model-validation/bin/run-and-validate.sh
+++ model-validation/bin/run-and-validate.sh
@@ -8,10 +8,13 @@ SCRIPT_ROOT=$(get_abs_path $(dirname $0))
 DEFAULTOSMT=${DEFAULTOSMT:-~/bin/opensmt}
 DEFAULTSCRAMBLER=${SCRAMBLER:-~/bin/scrambler}
 DEFAULTCHECKER=${CHECKER:-~/bin/ModelValidator}
-DEFAULTOUTDIR=./out/
 DEFAULTPRESERVE=false
 
-usage="Usage: $0 [-h] [-o <osmt2-binary>] [-s <scrambler>] [-c <checker>] [ -d <output-directory> ] [-p <true|false] <file>"
+TMPDIR=$(mktemp -d)
+DEFAULTOUTDIR=${TMPDIR}
+
+
+usage="Usage: $0 [-h] [-o <osmt2-binary>] [-s <scrambler>] [-c <checker>] [ -d <output-directory> ] [-p <true|false> ] <file>"
 
 while [ $# -gt 0 ]; do
     case $1 in
@@ -44,6 +47,11 @@ while [ $# -gt 0 ]; do
     shift; shift
 done
 
+if [ $# == 0 ]; then
+    echo $usage;
+    exit 1
+fi
+
 if [ -z ${binary} ]; then
     binary=${DEFAULTOSMT}
 fi
@@ -64,35 +72,45 @@ if [ -z ${preserve} ]; then
     preserve=${DEFAULTPRESERVE}
 fi
 
+trap "[ $preserve == true ] || rm -rf $TMPDIR" EXIT
 
-echo $1
+[ -x ${scrambler} ] || \
+    (echo "Scrambler not found or not executable: ${scrambler}"; exit 1)
 
-tmpin=$(tempfile -d /tmp -p tmp -s .smt2)
-tmpout=$(dirname ${tmpin})/$(basename ${tmpin} .smt2).out
+[ -x ${checker} ] || \
+    (echo "Checker not found or not executable: ${checker}"; exit 1)
+
+tmpin=${TMPDIR}/file.smt2
+tmpout=${TMPDIR}/file.out
 
 mkdir -p ${outdir}
 
-${scrambler} -seed "0" -gen-model-val true < $1 > ${tmpin}
+${scrambler} -seed "1" -gen-model-val true < $1 > ${tmpin}
 
 sh -c "\
     ulimit -St 10;
     ulimit -Sv 4000000
     ${binary} ${tmpin}" \
-        > ${tmpout}
+        > ${tmpout} 2>/dev/null
 
+output=${outdir}/$(basename $1 .smt2).out
 if [[ $(grep '^sat' ${tmpout}) ]]; then
     sh -c "\
         ulimit -St 10;
         ulimit -Sv 4000000;
         ${checker} --smt2 ${tmpin} --model ${tmpout}" \
-            > ${outdir}/$(basename $1 .smt2).out
+            > ${output}
+    [ $? -eq 152 ] || grep "model_validator_status=VALID" ${output} >/dev/null || \
+        (echo "Invalid model for $1"; exit 1)
 else
     echo "not sat"
+    exit 1
 fi
 
 if [[ ${preserve} == "true" ]]; then
     echo "Left the annotated instance and the model to ${tmpin} and ${tmpout}"
+    echo "Left the model validation output to ${output}"
 else
-    rm -rf ${tmpin} ${tmpout}
+    rm -rf ${tmpin} ${tmpout} ${output} ${outdir}
 fi
 
