ceaee85321e2b34b5a1aec12e8bc1c5309c48f74
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-06-08 21:32:47 +0200

    Remove ulimit on time and memory, SMTS server will handle both

--- smts/cluster/bin/make_scripts_smts.sh
+++ smts/cluster/bin/make_scripts_smts.sh
@@ -62,9 +62,7 @@ __EOF__
   echo $ex;
   inp=/tmp/\$(basename \${script})-`basename $ex .bz2`;
   bunzip2 -c $ex > \${inp};
-  sh -c "ulimit -St ${timeout};
-  ulimit -Sv 12000000;
-  /usr/bin/time -o \${smts_time}.${i}.time -f 'user: %U system: %S wall: %e CPU: %PCPU' python3 \$script -o3 -l -p $((port+i)) -fp \$inp" || true; rm \${inp};
+  sh -c "/usr/bin/time -o \${smts_time}.${i}.time -f 'user: %U system: %S wall: %e CPU: %PCPU' python3 \$script -o3 -l -p $((port+i)) -fp \$inp" || true; rm \${inp};
  ) > \$output.${i}.out 2> \$output.${i}.err;
  out_path=\$output.${i}
  grep '^;' \$out_path.out > /dev/null && (cat \$out_path.out >> \$out_path.err; echo error > \$out_path.out) &
